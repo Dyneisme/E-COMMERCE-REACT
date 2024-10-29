b787e79b5ca34dc102c4b25aa7c16c7e27dbd76e
Dyneisme <160685549+Dyneisme@users.noreply.github.com>
2024-10-29 15:13:46 +0800

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -11,6 +11,14 @@ const path = require('path');      // Import path for handling file paths
 // Initialize the app
 const app = express();
 
+// Custom CORS Headers
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://e-commerce-react-front-end.onrender.com");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth-token");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 // Configure CORS
 const allowedOrigins = [
   'https://e-commerce-react-front-end.onrender.com' // Production
