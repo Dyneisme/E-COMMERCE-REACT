5b06fe118d1b247f17d90a2c034da2e9adbc3bf4
Dyneisme <160685549+Dyneisme@users.noreply.github.com>
2024-10-29 13:46:37 +0800

    Update NewCollections.jsx

--- front-end/src/Components/NewCollections/NewCollections.jsx
+++ front-end/src/Components/NewCollections/NewCollections.jsx
@@ -8,7 +8,7 @@ const NewCollections = () => {
   const [new_collection,setNew_collection] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/new-collections')
+    fetch('https://e-commerce-react-xp0f.onrender.com/new-collections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
