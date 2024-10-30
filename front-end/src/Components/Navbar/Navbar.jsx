9364ff5657cbf50a3821031d749d901081b1a9ef
Dyneisme <160685549+Dyneisme@users.noreply.github.com>
2024-10-30 10:08:06 +0800

    Update Navbar.jsx

--- front-end/src/Components/Navbar/Navbar.jsx
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -50,11 +50,17 @@ const Navbar = () => {
         </li>
       </ul>
       <div className='nav-login-cart'>
-        {localStorage.getItem('auth-token')
-        ?<button onClick={()=> {localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
-        :<Link to='/login'><button>Login</button></Link>}
-        <Link to='/cart'><img src={cart_icon} alt='' /></Link>
-        <div className='nav-cart-count'>{getTotalCartItems()}</div>
+        {localStorage.getItem('auth-token') ? (
+          <button onClick={() => { localStorage.removeItem('auth-token'); window.location.replace('/'); }}>
+            Logout
+          </button>
+        ) : (
+          <Link to='/login'><button>Login</button></Link>
+        )}
+        <Link to='/cart'>
+          <img src={cart_icon} alt='Cart icon' />
+          {totalCartItems > 0 && <div className='nav-cart-count'>{totalCartItems}</div>}
+        </Link>
       </div>
     </div>
   );
